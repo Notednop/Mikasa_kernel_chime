621606411d7d4bc08dbe8b91a9924de978ffafbb
anothernop <notednop@gmail.com>
2026-07-07 20:39:55 +0000

    fix duplicate code

--- sound/usb/card.c
+++ sound/usb/card.c
@@ -328,7 +328,6 @@ static int snd_usb_create_streams(struct snd_usb_audio *chip, int ctrlif)
 		/* fall through */
 
 	case UAC_VERSION_2:
-	case UAC_VERSION_2:
 	case UAC_VERSION_1: {
 		struct uac1_ac_header_descriptor *h1;
 		int rest_bytes;
