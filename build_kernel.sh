0d3c4e4c0d044aeadd5b80195b96a9e42522abdc
anothernop <notednop@gmail.com>
2026-07-16 16:07:44 +0000

    Nethunter

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,19 +2,19 @@
 set -e
 
 [ ! -e "scripts/packaging/pack.sh" ] && git submodule init && git submodule update
-[ ! -e "toolchain" ] && echo "Make toolchain avaliable at $(pwd)/toolchain" && exit
+[ ! -e "toolchain" ] && echo "Make toolchain avaliable at /workspaces/anothernop/u" && exit
 
 # Patch for 4.14
 sed -i 's/#ifdef CONFIG_KPROBES/#if 0/g' KernelSU/kernel/ksu.c
 
-export KBUILD_BUILD_USER=shadow
-export KBUILD_BUILD_HOST=elite
+export KBUILD_BUILD_USER=anothernop
+export KBUILD_BUILD_HOST=mikasa
 
-PATH=$PWD/toolchain/bin:$PATH
+PATH=/workspaces/anothernop/bin:$PATH
 
 rm -rf out
 make O=out CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 -j$(nproc) vendor/nh_defconfig #chime_defconfig
 make O=out CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 -j$(nproc) 2>&1 menuconfig
-toilet -f future --filter border:metal BUILD START | lolcat
+toilet -f future --filter border:metal BUILD START
 make O=out CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 -j$(nproc) 2>&1 | tee ./out/build.log
-toilet -f future --filter border:metal BUILD END | lolcat
+toilet -f future --filter border:metal BUILD END 
